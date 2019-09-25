bed5c78c01be6403738b7c4352fc9ae8df298f20
sainath kollipara <ksn_mentos@yahoo.in>
2019-09-25 15:02:52 +0530

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../product.service';
+
+import { ProductService } from '../product.service';
+
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
@@ -8,7 +11,7 @@ import {ProductService} from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
